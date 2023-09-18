21748c24ababe3630067fc14b88d1df973baa35d
Cesar Guerrero <32390693+guer0157@users.noreply.github.com>
2023-09-18 15:08:06 +0100

    Week 1

---
+++ conditionalStatements.js
@@ -0,0 +1,28 @@
+//strings
+const str="Welcome"
+//numbers === intergets
+const num= 1
+// true or false values
+const booleanTrue = true; //truthy
+const booleanFalse = false; // falsey
+
+
+//ordered lists are called arrays
+// let, var and const 
+let arr = [];
+
+const sayYes = ()=>{
+    const numberToCheck = 10;
+//function block
+    if(numberToCheck === 2){
+        // if block
+        console.log("Number to check is equal to 2");
+    }else if(numberToCheck === 4){
+        console.log("Number to check is equal to 4")
+    }else{
+       //else block
+    console.log("Check failed because number is", numberToCheck)
+    }
+
+}//call the function
+sayYes()
---
+++ loops.js
@@ -0,0 +1,6 @@
+const runTheForLoop=()=>{
+    for(let i=0; i <= 5; i++ ){
+        console.log("What's the index at", i);
+    }
+}
+runTheForLoop();
